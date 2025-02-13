8339efd7f02bee1521f54e1969ebd1e2f9ab3c0b
MangoFanFan_ <mangofanfanw@icloud.com>
2025-02-13 16:50:10 +0800

    输出更多日志

--- app/view/tool_interface.py
+++ app/view/tool_interface.py
@@ -48,6 +48,7 @@ class ToolInterface(SmoothScrollArea):
         self.__initToolList()
 
         self.scrollLayout.addStretch()
+        logger.success("工具页面初始化完毕。")
 
     def __initToolInfo(self) -> None:
         n = 0
@@ -57,7 +58,7 @@ class ToolInterface(SmoothScrollArea):
                 self.toolInfoList.append(next(tools))
                 n += 1
         except StopIteration:
-            pass
+            logger.success(f"已经加载完毕所有工具，总数 {n}")
         return None
 
     def __initToolList(self) -> None:
@@ -78,6 +79,8 @@ class ToolInterface(SmoothScrollArea):
             widget.launchButton.clicked.connect(partial(self.launchTool, tool.name))
             widget.clicked.connect(partial(self.showInfoBox, tool))
             self.flowLayout.addWidget(widget)
+
+        logger.success("所有工具已经在工具页面显示。")
         return None
 
     def launchTool(self, toolName: str) -> None:
@@ -86,22 +89,31 @@ class ToolInterface(SmoothScrollArea):
         :param toolName: str，工具名称。
         :return: None
         """
+        logger.trace(f"准备启动工具 {toolName}")
         tool: Tool = None
         for t in self.toolInfoList:
             if t.name == toolName:
                 tool = t
                 break
         if tool is None:
+            logger.critical(f"启动工具时提供的工具名称无效：{toolName}。")
+            logger.critical(f"此错误不应该出现！")
             raise Exception(f"toolName={toolName} not found")
 
         # 分启动模式来启动工具
+        logger.info(f"开始启动工具 {toolName}")
         if tool.launchMode == 0:  # 直接 import 来启动
+            logger.trace(f"启动模式：0；工具 {toolName}：")
             if tool.module not in self.launchedToolInfoDict.keys():
+                logger.trace("工具箱启动后首次导入...")
                 self.launchedToolInfoDict[tool.module] = importlib.import_module(f"tool.{tool.module}.run")
             else:
+                logger.trace("重新导入...")
                 self.launchedToolInfoDict[tool.module] = importlib.reload(self.launchedToolInfoDict[tool.module])
         elif tool.launchMode == 1:
+            logger.trace(f"启动模式：1；工具 {toolName}：")
             if tool.module not in self.launchedToolInfoDict.keys():
+                logger.trace("正在创建子进程来运行工具...")
                 process = QProcess()
                 process.setEnvironment([f"{basicFunc.getHerePath()}/tool/{tool.module}"] + list(os.environ))
                 process.started.connect(lambda: self.processStarted(process, toolName))
@@ -112,14 +124,17 @@ class ToolInterface(SmoothScrollArea):
                 process.start(self.pythonRuntimePath, [f"{basicFunc.getHerePath()}/tool/{tool.module}/run.py"])
                 self.launchedToolInfoDict[tool.module] = process
             else:
+                logger.trace("工具正在运行中！")
                 self.infoBar.multi_launch_error(self.window())
                 logger.error(f"工具 {toolName} 无法多开。")
+        logger.trace("工具启动命令处理完毕。")
         return None
 
     def showInfoBox(self, tool: Tool) -> None:
         infoBox = ToolInfoBox(tool=tool, parent=self._parent)
         infoBox.launchToolSignal.connect(lambda: self.launchTool(tool.name))
         infoBox.show()
+        logger.info(f"打开工具 {tool.name} 的信息框。")
         return None
 
     @staticmethod
